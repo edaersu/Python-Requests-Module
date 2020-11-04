1b7ac0c8f10bfbc476d0cbf6a2bb39e6d5d1ae0c
edaersu <edaersu1@gmail.com>
2020-11-04 21:06:35 +0300

    read json

--- import requests.py
+++ import requests.py
@@ -18,7 +18,20 @@ print(requests.post(url))
 ## print(resp.status_code)
 ## print(resp.ok)
 
+import json
+import bios
+
+data = bios.read('categories.json')
+json_data = json.dumps(data)  ## converting dict to json
+categories_json = json.loads(json_data) 
+
+data2 = bios.read('quiz.json')
+json_data2 = json.dumps(data2)
+quiz_json = json.loads(json_data2)
+print(quiz_json)
+
+
 from firebase import firebase   ### pip install git+https://github.com/ozgur/python-firebase 
 firebase = firebase.FirebaseApplication('https://test-for-python-api.firebaseio.com', None)
-result = firebase.patch('/categories/', categoriesList)
-result2 = firebase.patch('/quiz/', quizList)
+result = firebase.patch('/categories/', categories_json)
+result2 = firebase.patch('/quiz/', quiz_json)

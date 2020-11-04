23fb44a56620a8b7f8b5868227710a7ca01d8e5a
edaersu <edaersu1@gmail.com>
2020-11-04 21:26:04 +0300

    edit

--- post.py
+++ post.py
@@ -1,17 +1,7 @@
 import requests
 
 url = "https://test-for-python-api.firebaseio.com/.json"
-categoriesList = {
-    "0": {"id":0,"summary":"summary1","title":"integral"},
-    "1": {"id":1,"summary":"summary2","title":"integral"},
-    "2": {"id":2,"summary":"summary3","title":"integral"},
-    
-}
-quizList = {
-    "0":{"category_id":0,"quiz":{"question":{"question":"question sentence","a1":"answer1","a2":"answer2","correct":"d"}}},
-    "1":{"category_id":1,"quiz":{"question":{"question":"question sentence","a1":"answer1","a2":"answer2","correct":"d"}}},
-    
-}
+
 print(requests.post(url))
 
 ## resp = requests.post(url, json=categoriesList) ## send requests with firebase key
@@ -33,6 +23,6 @@ print(quiz_json)
 
 
 from firebase import firebase   ### pip install git+https://github.com/ozgur/python-firebase 
-firebase = firebase.FirebaseApplication('https://test-for-python-api.firebaseio.com', None)
+firebase = firebase.FirebaseApplication(url, None)
 result = firebase.patch('/categories/', categories_json)
 result2 = firebase.patch('/quiz/', quiz_json)

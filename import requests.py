1ae8e829407894e50235b4bcfbb2bb8831cccb8e
edaersu <edaersu1@gmail.com>
2020-11-04 20:36:38 +0300

    edit

--- import requests.py
+++ import requests.py
@@ -8,12 +8,17 @@ categoriesList = {
     
 }
 quizList = {
-    "0":{"id":0,"quiz":{"question":{"soru":"question sentence","a1":"answer1","a2":"answer2","correct":"d"}}},
-    "1":{"id":1,"quiz":{"question":{"soru":"question sentence","a1":"answer1","a2":"answer2","correct":"d"}}},
+    "0":{"category_id":0,"quiz":{"question":{"question":"question sentence","a1":"answer1","a2":"answer2","correct":"d"}}},
+    "1":{"category_id":1,"quiz":{"question":{"question":"question sentence","a1":"answer1","a2":"answer2","correct":"d"}}},
     
 }
 print(requests.post(url))
-resp = requests.post(url, json=categoriesList)
 
-print(resp.status_code)
-print(resp.ok)
+## resp = requests.post(url, json=categoriesList) ## send requests with firebase key
+## print(resp.status_code)
+## print(resp.ok)
+
+from firebase import firebase   ### pip install git+https://github.com/ozgur/python-firebase 
+firebase = firebase.FirebaseApplication('https://test-for-python-api.firebaseio.com', None)
+result = firebase.patch('/categories/', categoriesList)
+result2 = firebase.patch('/quiz/', quizList)
